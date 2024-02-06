1870661d0cdd35f26735d51e37d4a706ddac18e5
Lucassamuel97 <lukassamuka88@gmail.com>
2024-02-06 09:41:58 -0300

    Criado entidade Cliente

---
+++ src/main/java/com/github/Lucassamuel97/clientes/model/entity/Cliente.java
@@ -0,0 +1,29 @@
+package com.github.Lucassamuel97.clientes.model.entity;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Data
+public class Cliente {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(nullable = false, length = 150)
+	private String nome;
+	
+	@Column(nullable = false, length = 150)
+	private String cpf;
+	
+	@Column
+	private LocalDate dataCadastro;
+}
